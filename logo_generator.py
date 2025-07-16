66e3bcf72b6c4c543bae0c40cfb6d363e5499eac
yodamendel <124314106+adams-charleen@users.noreply.github.com>
2025-07-16 19:05:13 -0400

    Update logo_generator.py

--- logo_generator.py
+++ logo_generator.py
@@ -1,3 +1,6 @@
+# Copyright (c) 2025 Charleen Adams
+# Licensed under the MIT License. See the LICENSE file for details.
+
 from iridescent_bg import generate_iridescent_background, add_text
 
 # Generate a wide, slightly taller background
